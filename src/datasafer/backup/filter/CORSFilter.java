49343191abd927f743011a307f4dfba8f354862d
Datasafer Senai <datasafer.senai@gmail.com>
2016-11-26 11:52:44 -0200

    Melhorias gerais

--- src/datasafer/backup/filter/CORSFilter.java
+++ src/datasafer/backup/filter/CORSFilter.java
@@ -9,8 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 @WebFilter(filterName = "CORSFilter")
@@ -20,15 +22,21 @@ public class CORSFilter implements Filter {
 	public void doFilter(	ServletRequest req,
 							ServletResponse resp,
 							FilterChain chain)
-			throws IOException, ServletException {
+												throws IOException, ServletException {
 
 		HttpServletResponse response = (HttpServletResponse) resp;
+		HttpServletRequest request = (HttpServletRequest) req;
 
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, usuario, estacao, backup, operacao");
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		
+
+		if (request.getMethod().equals(HttpMethod.OPTIONS)) {
+			response.setHeader("Allow", "GET,POST,PUT,DELETE,OPTIONS");
+			return;
+		}
+
 		chain.doFilter(req, resp);
 
 	}
